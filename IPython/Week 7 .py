05662ef4e82ea2ca7ea9601770d7e9e60c2a1f58
geemsda <iamclassygc@aol.com>
2014-07-20 20:04:14 -0400

    Update Week 7 .py

--- IPython/Week 7 .py
+++ IPython/Week 7 .py
@@ -1,19 +1,4 @@
 
-# coding: utf-8
-
-# In[4]:
-
-import urllib2
-
-search_url = 'http://fishbase.se/search.php'
-search_page = urllib2.urlopen(search_url)
-
-print search_page.read(200)
-
-soup = BeautifulSoup(search_page)
-
-
-# In[14]:
 
 import urllib2
 from bs4 import BeautifulSoup
